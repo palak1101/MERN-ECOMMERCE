77905aa766707f474d3784309bf7c404c19f4d4c
Palak <palaktiwari156@gmail.com>
2022-02-26 20:01:08 +0530

    razorpay integration

--- my-app/src/components/cart/CartOrderSummary.js
+++ my-app/src/components/cart/CartOrderSummary.js
@@ -10,6 +10,7 @@ import {
 import * as React from 'react'
 import { FaArrowRight } from 'react-icons/fa'
 import { formatPrice } from './PriceTag'
+import axios from 'axios'
 
 
 const OrderSummaryItem = (props) => {
@@ -25,6 +26,85 @@ const OrderSummaryItem = (props) => {
 }
 
 export const CartOrderSummary = ({ total }) => {
+
+    const loadScript = React.useCallback((src) => {
+        return new Promise((resolve) => {
+            const script = document.createElement("script");
+            script.src = src;
+            script.onload = () => {
+                resolve(true);
+            };
+            script.onerror = () => {
+                resolve(false);
+            };
+            document.body.appendChild(script);
+        });
+    }, [])
+
+    const handleOrder = async () => {
+        const res = await loadScript(
+            "http://checkout.razorpay.com/v1/checkout.js"
+        );
+
+        if (!res) {
+            alert("Razorpay SDK failed to load. Are you online?");
+            return;
+        }
+
+        // creating a new order
+        const result = await axios.post("http://localhost:8080/api/v1/order/createOrder", {
+            amount: '50000'
+        });
+
+        if (!result) {
+            alert("Server error. Are you online?");
+            return;
+        }
+
+        // Getting the order details back
+        const { amount, id: order_id, currency } = result.data;
+
+        const options = {
+            key: "rzp_test_AsdCb1DBwZJBEA", // Enter the Key ID generated from the Dashboard
+            amount: amount.toString(),
+            currency: currency,
+            name: "+Plus.",
+            description: "Test Transaction",
+            image: 'https://www.svgrepo.com/show/174895/orkut-logo.svg',
+            order_id: order_id,
+            handler: async function (response) {
+                const data = {
+                    orderCreationId: order_id,
+                    razorpayPaymentId: response.razorpay_payment_id,
+                    razorpayOrderId: response.razorpay_order_id,
+                    razorpaySignature: response.razorpay_signature,
+                };
+
+                const result = await axios.post("http://localhost:8080/api/v1/order/verify", data);
+
+                alert(result.data.msg);
+            },
+            prefill: {
+                name: "Palak Tiwari",
+                email: "palaktiwari156@gmail.com",
+                contact: "9999999999",
+            },
+            notes: {
+                address: "Kamptee Road",
+            },
+            theme: {
+                color: "#61dafb",
+            },
+        };
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+    }
+
+
+
+
+
     return (
         <Stack spacing="8" borderWidth="1px" rounded="lg" padding="8" width="full">
             <Heading size="md">Order Summary</Heading>
@@ -50,7 +130,7 @@ export const CartOrderSummary = ({ total }) => {
                     </Text>
                 </Flex>
             </Stack>
-            <Button colorScheme="blue" size="lg" fontSize="md" rightIcon={<FaArrowRight />}>
+            <Button onClick={handleOrder} colorScheme="blue" size="lg" fontSize="md" rightIcon={<FaArrowRight />}>
                 Checkout
             </Button>
         </Stack>
